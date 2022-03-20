405816c3cb93f11ab187e97de1c0ee87a5e718de
CarsonLosch <95552371+CarsonLosch@users.noreply.github.com>
2022-03-20 13:39:02 -0700

    done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  let form = document.querySelector("form")
+  form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    buildToDo(event.target.new_todo.value);
+    form.reset();
+  })
 });
+
+function buildToDo(todo) {
+  let list = document.createElement('h4')
+  let btn = document.createElement('button')
+  btn.addEventListener('click', handleDelete )
+  btn.textContent = 'x'
+  list.textContent = `${todo} `
+  list.appendChild(btn)
+  console.log(list);
+  document.getElementById('tasks').appendChild(list);
+  
+}
+
+function handleDelete(event) {
+  event.target.parentNode.remove();
+}
